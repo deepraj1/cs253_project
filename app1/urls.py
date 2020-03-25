46402ba569b685ca7bd0904b252148ec8b833515
nivedh1 <nivedh@iitk.ac.in>
2020-03-26 03:55:17 +0530

    ta_dashboard added

--- app1/urls.py
+++ app1/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from . import views
 
-app_name = "myapp"
+appname = "myapp"
 
 urlpatterns = [
-    path('upload', views.upload,name='upload'),
+    path('upload', views.upload, name='upload'),
     path('register', views.register, name='register'),
-    path('login', views.login ,name='login'),
-    #path('dashboard',views.dashboard,name='dashboard')
-    #path('registration',views.regis,name='registration'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('student/<int:student_id>', views.student, name='student')
+
 ]
+
